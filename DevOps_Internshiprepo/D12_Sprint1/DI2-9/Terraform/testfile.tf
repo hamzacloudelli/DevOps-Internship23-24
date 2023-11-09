cf0673e645234b33568f11d1e0c7abbbf80571d8
hamzacloudelli <149590322+hamzacloudelli@users.noreply.github.com>
2023-11-09 01:38:39 -0800

    Add files via upload

---
+++ DevOps_Internshiprepo/D12_Sprint1/DI2-9/Terraform/testfile.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+   volumes {
+    host_path      = "C:/Users/HP/Downloads/index.html"  
+    container_path = "/usr/share/nginx/html/index.html"
+  }
+}
