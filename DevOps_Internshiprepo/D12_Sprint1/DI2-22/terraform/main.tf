9a1f27c2ba2085f68091ceaa9b51175240ab8698
hamzacloudelli <149590322+hamzacloudelli@users.noreply.github.com>
2023-11-21 01:10:42 +0500

    Add files via upload

--- DevOps_Internshiprepo/D12_Sprint1/DI2-22/terraform/main.tf
+++ DevOps_Internshiprepo/D12_Sprint1/DI2-22/terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -9,6 +5,22 @@ resource "aws_vpc" "vpc" {
     Name = "hamzavpcproj"
   }
 }
+
+# elastic ip association
+resource "aws_eip_association" "hamza_eip_assoc" {
+  instance_id = aws_instance.web.id
+}
+
+# elastic ip association
+resource "aws_eip_association" "hamza_eip_assoc2" {
+  instance_id = aws_instance.web2.id
+  allocation_id = aws_eip.example.id
+}
+
+resource "aws_eip" "example" {
+  domain = "vpc"
+}
+
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
@@ -18,6 +30,16 @@ resource "aws_subnet" "public_subnet" {
 
   tags = {
     Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "public_subnet2" {
+  vpc_id           = aws_vpc.vpc.id
+  cidr_block       = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "another-private-subnet"
   }
 }
 
@@ -57,6 +79,12 @@ resource "aws_route_table_association" "public_subnet_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+resource "aws_route_table_association" "public_subnet_route_table_association2" {
+  subnet_id = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
 resource "aws_security_group" "webssh" {
   name        = "webssh"
   description = "Allow SSH from local IP only"
@@ -90,57 +118,57 @@ resource "aws_security_group" "webssh" {
   }
 }
 
-/*
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.webssh.id]
-  
-  tags = {
-    Name = "hamzaproj3"
-  }
-}
-*/
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webssh.id]
 
+  user_data = <<-EOF
+
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install ec2-instance-connect
+    sudo yum install docker -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo docker pull nginx
+    sudo docker run -d -p 80:80 nginx
+    
+    EOF
+
   tags = {
     Name = "hamzaproj3"
   }
 }
 
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet2.id
+  vpc_security_group_ids = [aws_security_group.webssh.id]
   
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
+  user_data = <<-EOF
+
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install ec2-instance-connect
+    sudo yum install docker -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo docker pull nginx
+    sudo docker run -d -p 80:80 nginx
+    
+    EOF
+
+  tags = {
+    Name = "hamzaproj31"
   }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
 }
 
-output "instanceVal1"{
 
-    value = "Instance is Launcher -> ${aws_instance.web.availability_zone}"
 
-}
-
-output "instanceVal2"{
-
-    value = "ID of the Instance -> ${aws_instance.web.id}"
-
-}
 
 resource "aws_ebs_volume" "volume" {
 
@@ -148,24 +176,12 @@ resource "aws_ebs_volume" "volume" {
     availability_zone = aws_instance.web.availability_zone
 
 // Size IN GiB
-    size = 10
+    size = 1
 
     tags = {
 
         Name = "terraformTesting"
     }    
-}
-
-output "volumeVal1" {
-
-    value = "AZ of volume -> ${aws_ebs_volume.volume.availability_zone}"
-    
-}
-
-output "volumeVal2" {
-
-    value = "ID of Volume -> ${aws_ebs_volume.volume.id}"
-
 }
 
 #attaching the volume to ec2 instance 
@@ -176,4 +192,16 @@ resource "aws_volume_attachment" "ebsAttach" {
     volume_id = aws_ebs_volume.volume.id
     instance_id = aws_instance.web.id
 
+}
+
+resource "aws_efs_file_system" "efs_fs" {
+  performance_mode = "generalPurpose"
+  throughput_mode = "bursting"
+}
+
+resource "aws_efs_mount_target" "efs_mount_target" {
+  file_system_id = aws_efs_file_system.efs_fs.id
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.webssh.id]
+  depends_on = [aws_efs_file_system.efs_fs]
 }
